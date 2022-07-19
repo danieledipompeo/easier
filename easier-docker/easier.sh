4c672ca05d08c979c5591e34c41167fa8808907e
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2022-07-19 11:26:39 +0200

    update easier script

--- easier-docker/easier.sh
+++ easier-docker/easier.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+get_config_param() {
+  grep -v '^#' config.ini | grep -Pzo "\-${1}\n\K[^\n]+"
+}
+
+CASE_STUDY=$(get_config_param m | cut -d'/' -f2)
+ALGORITHM=$(get_config_param algo)
+OUT_DIR=$(get_config_param outF)
+
 wget -O config.ini $1
-java -Xmx12g -jar easier.jar @./config.ini
+java -Xmx12g -jar easier.jar @./config.ini 2>&1 | tee "${OUT_DIR}/easier__${CASE_STUDY}__${ALGORITHM}.log"
