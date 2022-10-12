7deccb8136b921dbb086eed26a10f881d6e37056
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2022-10-12 10:06:05 +0200

    Update Dockerfile and the script
    fix some issues in the dockerfile and how the script is executed

--- easier-docker/easier.sh
+++ easier-docker/easier.sh
@@ -4,9 +4,12 @@ get_config_param() {
   grep -v '^#' config.ini | grep -Pzo "\-${1}\n\K[^\n]+"
 }
 
+wget -O config.ini $1
+
 CASE_STUDY=$(get_config_param m | cut -d'/' -f2)
 ALGORITHM=$(get_config_param algo)
 OUT_DIR=$(get_config_param outF)
 
-wget -O config.ini $1
+mkdir -p $OUT_DIR
+cp config.ini $OUT_DIR
 java -Xmx12g -jar easier.jar @./config.ini 2>&1 | tee "${OUT_DIR}/easier__${CASE_STUDY}__${ALGORITHM}.log"
