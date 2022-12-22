ffee46ac57c677389fedc7ec0bb72a3139030026
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2022-12-22 12:54:46 +0100

    update docker build

--- easier-docker/easier.sh
+++ easier-docker/easier.sh
@@ -25,4 +25,6 @@ wget -O /opt/easier/easier-uml2lqnCaseStudy/$(get_config_param initialModelPath)
 
 mkdir -p $OUT_DIR
 cp config.ini $OUT_DIR
-java -Xmx12g -jar easier.jar @./config.ini 2>&1 | tee "${OUT_DIR}/easier__${CASE_STUDY}__${ALGORITHM}.log"
+JVM_PARAMS="-Xmx12g --add-exports java.xml/com.sun.org.apache.xerces.internal.dom=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=${OUT_DIR}/easier-oom -XX:StartFlightRecording=filename=${OUT_DIR}/easier.jfr,path-to-gc-roots=true,settings=profile"
+
+java -jar ${JVM_PARAMS} easier.jar @./config.ini 2>&1 | tee "${OUT_DIR}/easier__${CASE_STUDY}__${ALGORITHM}.log"
