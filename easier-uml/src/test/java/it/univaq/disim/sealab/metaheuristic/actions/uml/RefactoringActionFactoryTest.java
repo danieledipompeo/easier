334a2416d837eb02ec0be1c693b2a017216b7996
Daniele Di Pompeo <dipompeodaniele@gmail.com>
2023-08-25 13:01:08 +0200

    fix error with temp files

--- easier-uml/src/test/java/it/univaq/disim/sealab/metaheuristic/actions/uml/RefactoringActionFactoryTest.java
+++ easier-uml/src/test/java/it/univaq/disim/sealab/metaheuristic/actions/uml/RefactoringActionFactoryTest.java
@@ -4,11 +4,19 @@ import it.univaq.disim.sealab.metaheuristic.actions.RefactoringAction;
 import it.univaq.disim.sealab.metaheuristic.evolutionary.RSolution;
 import it.univaq.disim.sealab.metaheuristic.evolutionary.UMLRProblem;
 import it.univaq.disim.sealab.metaheuristic.evolutionary.UMLRSolution;
+import it.univaq.disim.sealab.metaheuristic.utils.Configurator;
 import it.univaq.disim.sealab.metaheuristic.utils.EasierException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -19,23 +27,44 @@ public class RefactoringActionFactoryTest {
 
     UMLRSolution sol;
 
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception {
+        Files.createDirectories(Configurator.eINSTANCE.getTmpFolder());
+        Files.createDirectories(Configurator.eINSTANCE.getOutputFolder());
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws IOException {
+        Files.walk(Configurator.eINSTANCE.getOutputFolder())
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
+        Files.walk(Configurator.eINSTANCE.getTmpFolder())
+                .sorted(Comparator.reverseOrder())
+                .map(Path::toFile)
+                .forEach(File::delete);
+    }
+
     @BeforeEach
     public void init() {
         int allowedFailures = 100;
         int desired_length = 4;
         int populationSize = 4;
 
-        String modelpath = getClass().getResource("/models/model/automatedGuidedVehicle.uml").getFile();
-        UMLRProblem<RSolution<?>> p = new UMLRProblem<>(Paths.get(modelpath), "agv__test");
+        String modelpath = getClass().getResource("/simplified-cocome/cocome.uml").getFile();
+        UMLRProblem<RSolution<?>> p = new UMLRProblem<>(Paths.get(modelpath), "ccm__test");
         sol = (UMLRSolution) p.createSolution();
     }
 
     @Test
     public void getRandomActionTest() throws EasierException {
-        RefactoringAction action = RefactoringActionFactory.getRandomAction(sol.getVariable(0).getEasierModel().getAvailableElements(),
-				sol.getVariable(0).getEasierModel().getInitialElements());
-        assertNotNull(action);
-        assertFalse(action.getTargetElements().values().stream().flatMap(Set::stream).collect(Collectors.toSet()).isEmpty());
+        RefactoringAction action =
+                RefactoringActionFactory.getRandomAction(sol.getVariable(0).getEasierModel().getAvailableElements(),
+                        sol.getVariable(0).getEasierModel().getInitialElements());
+        assertNotNull(action, "The action should not be null");
+        assertFalse(
+                action.getTargetElements().values().stream().flatMap(Set::stream).collect(Collectors.toSet()).isEmpty(),
+                "The refactoring action should have at least one target element");
     }
 
 }
